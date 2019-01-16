4a4c947f55874d78b57990c6a482909f13d82ceb
Jack Leslie <jjdl@st-andrews.ac.uk>
2019-01-16 00:22:52 +0000

    Integrated Contentful for photos page

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
---
+++ src/pages/photos/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
+
+import Layout from '../../components/layout'
+import SEO from '../../components/seo'
+
+import photoStyles from './photos.module.css'
+
+const PhotosPage = ({ data }) => (
+  <Layout colour="yellow" active="photos">
+    <SEO title="Photos" />
+    <h1>Photos</h1>
+    <div className={photoStyles.container}>
+      {data.contentfulGallery.photos.map(photo => (
+        <Img className={photoStyles.photo} fluid={photo.fluid} />
+      ))}
+    </div>
+  </Layout>
+)
+
+export default PhotosPage
+
+export const pageQuery = graphql`
+  query TextQuery {
+    contentfulGallery {
+      photos {
+        fluid(maxWidth: 600) {
+          ...GatsbyContentfulFluid
+        }
+      }
+    }
+  }
+`
