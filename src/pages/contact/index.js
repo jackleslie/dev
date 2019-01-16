f45e3fa3d791ca9217d996f4d3c81f86a9bd313d
Jack Leslie <jjdl@st-andrews.ac.uk>
2019-01-16 16:16:26 +0000

    Added bot field input

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -40,6 +40,7 @@ const ContactPage = () => (
       data-netlify="true"
       data-netlify-honeypot="bot-field"
     >
+      <input type="hidden" name="bot-field" />
       <div className={contactStyles.contactElement}>
         <input
           className={contactStyles.contactInput}
