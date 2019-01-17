dde15a97833383e80ad1370b7723917cbab63172
Jack Leslie <jjdl@st-andrews.ac.uk>
2019-01-17 14:57:51 +0000

    Longer project descriptions

--- src/components/project/index.js
+++ src/components/project/index.js
@@ -4,16 +4,16 @@ import PropTypes from 'prop-types'
 import projectStyles from './project.module.css'
 
 const Project = ({ project, colour }) => (
-  <a target="_blank" href={project.link}>
-    <div className={`${projectStyles.container} ${projectStyles[colour]}`}>
-      <h3 className={projectStyles.projectTitle}>{project.title}</h3>
-      <h4 className={projectStyles.projectDescription}>
-        {project.description}
-      </h4>
-      <h5 className={projectStyles.projectAward}>{`🏆${project.award} @ ${
-        project.event
-      } | ${project.issuer}`}</h5>
-    </div>
+  <a
+    className={`${projectStyles.container} ${projectStyles[colour]}`}
+    target="_blank"
+    href={project.link}
+  >
+    <h3 className={projectStyles.projectTitle}>{project.title}</h3>
+    <h4 className={projectStyles.projectDescription}>{project.description}</h4>
+    <h5 className={projectStyles.projectAward}>{`🏆${project.award} @ ${
+      project.event
+    } | ${project.issuer}`}</h5>
   </a>
 )
 
