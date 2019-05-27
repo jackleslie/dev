7e4e41171704620ff8f6fade6e58ce1d73a610d0
Jack Leslie <jjdl@st-andrews.ac.uk>
2019-05-27 13:48:04 +0100

    Added flag to config to cache contentful assets in development

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,7 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        downloadLocal: true,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
