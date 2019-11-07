5bf09758452b39047ff77e55c59f75fbdcb8ced6
Jack Leslie <jjdl@st-andrews.ac.uk>
2019-11-07 14:11:19 +0000

    Removed classnames

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Head from "next/head";
-import classnames from "classnames";
 
 import { useDarkMode } from "../hooks";
 
@@ -27,7 +26,7 @@ export default () => {
           onClick={toggleDark}
         >
           <div className="sun" />
-          <div className={classnames("moon", { crescent: dark })} />
+          <div className="moon" />
         </button>
       </article>
 
