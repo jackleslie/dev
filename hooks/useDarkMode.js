bc985e941d2d4840ac3408a5b62734a2821104c5
Jack Leslie <jjdl@st-andrews.ac.uk>
2019-11-07 15:50:03 +0000

    Hacky localStorage dark mode

--- hooks/useDarkMode.js
+++ hooks/useDarkMode.js
@@ -1,11 +1,26 @@
 /* global document */
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const useDarkMode = () => {
-  const [dark, toggleDark] = useState(false);
+  const [dark, toggleDark] = useState(null);
+
+  useEffect(() => {
+    const darkMode = window.localStorage.getItem("jackleslie-dark-mode");
+    if (darkMode == 1) {
+      document.documentElement.classList.add("dark-init");
+      document.documentElement.classList.add("dark");
+      toggleDark(true);
+    } else {
+      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.remove("dark-init");
+      toggleDark(false);
+    }
+  }, []);
 
   const update = () => {
+    document.documentElement.classList.remove("dark-init");
     document.documentElement.classList.toggle("dark");
+    window.localStorage.setItem("jackleslie-dark-mode", !dark ? 1 : 0);
     toggleDark(!dark);
   };
 
--- pages/index.js
+++ pages/index.js
@@ -9,49 +9,57 @@ export default () => {
   const themeColor = dark ? "#222222" : "#ffffff";
   const icon = dark ? "dark.ico" : "light.ico";
 
-  return (
-    <main>
-      <Head>
-        <title>Jack Leslie</title>
-        <meta name="theme-color" content={themeColor} />
-        <link rel="icon" href={icon} />
-      </Head>
+  let page;
 
-      <article>
-        <h1>Jack Leslie</h1>
-        <button
-          aria-label="Toggle Theme"
-          title="Toggle Theme"
-          type="button"
-          onClick={toggleDark}
-        >
-          <div className="sun" />
-          <div className="moon" />
-        </button>
-      </article>
+  if (dark !== null) {
+    page = (
+      <main>
+        <Head>
+          <title>Jack Leslie</title>
+          <meta name="theme-color" content={themeColor} />
+          <link rel="icon" href={icon} />
+        </Head>
 
-      <p>
-        Final year Computer Science student at the University of St Andrews,
-        Frontend developer and JavaScript enthusiast.
-      </p>
-      <p>
-        I was an intern at{" "}
-        <a href="https://transferwise.com" className="transferwise">
-          TransferWise
-        </a>{" "}
-        and will be returning as a graduate Fullstack developer.
-      </p>
-      <p>
-        If you want to get in touch you can{" "}
-        <a href="mailto:jackleslie@protonmail.com" className="protonmail">
-          email
-        </a>{" "}
-        me, and you can find me online through{" "}
-        <a href="https://keybase.io/jackleslie" className="keybase">
-          Keybase
-        </a>
-        .
-      </p>
-    </main>
-  );
+        <article>
+          <h1>Jack Leslie</h1>
+          <button
+            aria-label="Toggle Theme"
+            title="Toggle Theme"
+            type="button"
+            onClick={toggleDark}
+          >
+            <div className="sun" />
+            <div className="moon" />
+          </button>
+        </article>
+
+        <p>
+          Final year Computer Science student at the University of St Andrews,
+          Frontend developer and JavaScript enthusiast.
+        </p>
+        <p>
+          I was an intern at{" "}
+          <a href="https://transferwise.com" className="transferwise">
+            TransferWise
+          </a>{" "}
+          and will be returning as a graduate Fullstack developer.
+        </p>
+        <p>
+          If you want to get in touch you can{" "}
+          <a href="mailto:jackleslie@protonmail.com" className="protonmail">
+            email
+          </a>{" "}
+          me, and you can find me online through{" "}
+          <a href="https://keybase.io/jackleslie" className="keybase">
+            Keybase
+          </a>
+          .
+        </p>
+      </main>
+    );
+  } else {
+    page = null;
+  }
+
+  return page;
 };
