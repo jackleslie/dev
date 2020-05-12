f6eb33af63b78d294bfece933fcaa8125369f4c0
Jack Leslie <jjdl@st-andrews.ac.uk>
2020-05-12 14:52:12 +0100

    chore: add eslint

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import "../style.css";
+import React from 'react';
+import '../style.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
-/* global document window */
-import React from "react";
-import Head from "next/head";
+import React from 'react';
+import Head from 'next/head';
 
 export default () => {
   const toggle = () => {
-    const isDark = window.localStorage.getItem("jackleslie-dark-mode") != 0;
-    window.localStorage.setItem("jackleslie-dark-mode", isDark ? 0 : 1);
-    document.documentElement.classList.toggle("dark");
+    const isDark = window.localStorage.getItem('jackleslie-dark-mode') != 0;
+    window.localStorage.setItem('jackleslie-dark-mode', isDark ? 0 : 1);
+    document.documentElement.classList.toggle('dark');
   };
 
   return (
@@ -17,34 +16,29 @@ export default () => {
 
       <article>
         <h1>Jack Leslie</h1>
-        <button
-          aria-label="Toggle Theme"
-          title="Toggle Theme"
-          type="button"
-          onClick={toggle}
-        >
+        <button aria-label="Toggle Theme" title="Toggle Theme" type="button" onClick={toggle}>
           <div aria-label="Sun" className="sun" />
           <div aria-label="Moon" className="moon" />
         </button>
       </article>
 
       <p>
-        Final year Computer Science student at the University of St Andrews,
-        frontend developer and JavaScript enthusiast.
+        Final year Computer Science student at the University of St Andrews, frontend developer and
+        JavaScript enthusiast.
       </p>
       <p>
-        I was an intern at{" "}
+        I was an intern at{' '}
         <a href="https://transferwise.com" className="transferwise">
           TransferWise
-        </a>{" "}
+        </a>{' '}
         and will be returning as a graduate software engineer.
       </p>
       <p>
-        If you want to get in touch you can{" "}
+        If you want to get in touch you can{' '}
         <a href="mailto:jackleslie@protonmail.com" className="protonmail">
           email
-        </a>{" "}
-        me, and you can find me online through{" "}
+        </a>{' '}
+        me, and you can find me online through{' '}
         <a href="https://keybase.io/jackleslie" className="keybase">
           Keybase
         </a>
