5e776c54622bdf6dbe1de20f87390c9061114824
Jack Leslie <jjdl@st-andrews.ac.uk>
2020-06-27 16:01:36 +0100

    fix: rename some basic tests

--- components/header/header.test.js
+++ components/header/header.test.js
@@ -4,12 +4,12 @@ import Header from './header';
 
 const defaultTitle = 'Jack Leslie';
 
-test('renders default title with no prop', () => {
+test('renders home page name as title by default', () => {
   render(<Header />);
   expect(screen.getByText(defaultTitle)).toBeInTheDocument();
 });
 
-test('renders passed title prop with preceding slash', () => {
+test('renders blog page name with preceding slash', () => {
   const title = 'Blog';
   const renderedTitle = '/ Blog';
   render(<Header title={title} />);
