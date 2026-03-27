e953b9b0e8d39656dda512196b94613eee8a8caa
Jack Leslie <52004409+jackleslie@users.noreply.github.com>
2026-03-27 10:33:58 +0000

    update email

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 import { Layout } from "../components/layout/layout";
 
 export default function Index() {
@@ -10,12 +9,11 @@ export default function Index() {
         <a href="https://wise.com" className="wise">
           Wise
         </a>{" "}
-        working in the Send product team. I also sometimes take photos;
-        you can look at them on my <Link href="/gallery">gallery</Link>.
+        working in the Send product team.
       </p>
       <p>
         If you want to get in touch you can{" "}
-        <a href="mailto:jackjdleslie@gmail.com" className="gmail">
+        <a href="mailto:info@jackleslie.dev" className="gmail">
           email
         </a>{" "}
         me.
